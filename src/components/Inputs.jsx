7dadf065789971a60790841ddd2eb021d18a4065
theyashpatel <yashpatel26@gmail.com>
2022-05-31 16:30:09 +0530

    last d

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
 
 function Inputs() {
-  return <div>Inputs</div>;
+  return (
+    <div className="flex flex-row justify-center my-6">
+      <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+        <input
+          type="text"
+          placeholder="Search for city...."
+          className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
+        />
+        <UilSearch
+          size={25}
+          className="text-white cursor-pointer transition ease-out hover:scale-125"
+        />
+        <UilLocationPoint
+          size={25}
+          className="text-white cursor-pointer transition ease-out hover:scale-125"
+        />
+      </div>
+
+      <div className="flex flex-row w-1/4 items-center justify-center">
+        <button name="metric" className="text-xl text-white font-light">
+          °C
+        </button>
+        <p className="text-xl text-white mx-1">|</p>
+        <button name="imperial" className="text-xl text-white font-light">
+          °F
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Inputs;
