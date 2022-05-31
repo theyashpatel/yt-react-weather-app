1bb0c1c8df320d94ffeaab10ffaa63196c25cbf1
theyashpatel <yashpatel26@gmail.com>
2022-05-31 16:17:20 +0530

    last c

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import TopButtons from "./components/TopButtons";
+import Inputs from "./components/Inputs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
+      <Inputs />
     </div>
   );
 }
---
+++ src/components/Inputs.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Inputs() {
+  return <div>Inputs</div>;
+}
+
+export default Inputs;
